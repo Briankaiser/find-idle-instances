a093e69779b3975d2b4dd936c71a2b3ab928e931
Brian Kaiser <brian.kaiser@hudl.com>
2016-02-15 10:40:01 -0500

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,320 @@
+﻿using Amazon;
+using Amazon.CloudWatch;
+using Amazon.CloudWatch.Model;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Microsoft.Framework.Runtime.Common.CommandLine;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FixStuckWorkers
+{
+    internal class InstanceResult
+    {
+        public string Name { get; private set; }
+        public double Cpu { get; private set; }
+        public InstanceResult(string name, double cpu)
+        {
+            Name = name;
+            Cpu = cpu;
+        }
+        public override string ToString()
+        {
+            return Name + "\t\t" + Cpu.ToString("0.00");
+        }
+    }
+    internal class InstanceResults
+    {
+        public ConcurrentBag<InstanceResult> BadInstances { get; set; }
+        public ConcurrentBag<InstanceResult> GoodInstances { get; set; }
+        public ConcurrentBag<InstanceResult> UnknownInstances { get; set; }
+        public InstanceResults()
+        {
+            BadInstances = new ConcurrentBag<InstanceResult>();
+            GoodInstances = new ConcurrentBag<InstanceResult>();
+            UnknownInstances = new ConcurrentBag<InstanceResult>();
+        }
+    }
+    public class Program
+    {
+        private const double MaxBadCpuPercent = 1.0;
+        private const double UnknownCpuPercent = -1;
+        private const int MaxMetricConcurrency = 8;
+        private const int MetricRetrievalInHours = 3;
+        private const int PeriodInMinutes = 10;
+        //require at least 6 periods (60 minutes) to judge good or bad
+        private const int MinServerMetricsToJudge = 6;
+
+        private static string AccessKey { get; set; }
+        private static string SecretKey { get; set; }
+        private static string Name { get; set; }
+        private static RegionEndpoint Region { get; set; }
+        private static bool ShowGoodInstances { get; set; }
+        private static bool ShowUnknownInstances { get; set; }
+
+        public static int Main(string[] args)
+        {
+
+
+            var app = new CommandLineApplication
+            {
+                Name = "FixStuckWorkers",
+                Description = string.Format("Analyzes and fixes 'stuck' workers in Hudl encoder pools. Stuck is defined as less than {0}% CPU for {1} hours.",
+                    MaxBadCpuPercent, MetricRetrievalInHours),
+                FullName = "FixStuckWorkers - fixing your jank/frozen AWS servers since 2016"
+            };
+
+
+            //if no command - show help
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 2;
+            });
+
+            app.Command("find", c =>
+            {
+                c.Description = "Finds stuck instances";
+                var accessOption = c.Option("--accesskey", "AWS Access Key (required)", CommandOptionType.SingleValue);
+                var secretOption = c.Option("--secretkey", "AWS Secret Key (required)", CommandOptionType.SingleValue);
+                var nameOption = c.Option("--name", "Name of worker to search on (required)", CommandOptionType.SingleValue);
+                var regionOption = c.Option("--region", "AWS region name (optional)", CommandOptionType.SingleValue);
+                var showGoodOption = c.Option("--show-good", "Display 'good' instances in results (optional)", CommandOptionType.NoValue);
+                var showUnknownOption = c.Option("--show-unknown", "Display 'unknown' instances in results (optional)", CommandOptionType.NoValue);
+
+                c.OnExecute(async () =>
+                {
+                    bool success = ValidateAndSetOptions(accessOption, secretOption, nameOption, regionOption, showGoodOption, showUnknownOption);
+                    if(!success)
+                    {
+                        c.ShowHelp("find");
+                        return 1;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Running ({0})...", Name);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    var instanceResults = await FindInstancesAndCategorize();
+                    DisplayInstanceResults(instanceResults);
+
+                    return 0;
+                });
+
+
+            });
+            app.Command("terminate", c =>
+            {
+                c.Description = "TODO: Terminates stuck instances.";
+                var accessOption = c.Option("--accesskey", "AWS Access Key (required)", CommandOptionType.SingleValue);
+                var secretOption = c.Option("--secretkey", "AWS Secret Key (required)", CommandOptionType.SingleValue);
+                var nameOption = c.Option("--name", "Name of worker to search on (required)", CommandOptionType.SingleValue);
+                var regionOption = c.Option("--region", "AWS region name (optional)", CommandOptionType.SingleValue);
+                var showGoodOption = c.Option("--show-good", "Display 'good' instances in results (optional)", CommandOptionType.NoValue);
+                var showUnknownOption = c.Option("--show-unknown", "Display 'unknown' instances in results (optional)", CommandOptionType.NoValue);
+
+                c.OnExecute(async () =>
+                {
+                    bool success = ValidateAndSetOptions(accessOption, secretOption, nameOption, regionOption, showGoodOption, showUnknownOption);
+                    if (!success)
+                    {
+                        c.ShowHelp("terminate");
+                        return 1;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Running ({0})...", Name);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    var instanceResults = await FindInstancesAndCategorize();
+                    DisplayInstanceResults(instanceResults);
+
+                    //confirm terminate terminate
+
+                    //terminate instances
+
+                    return 0;
+                });
+            });
+
+            return app.Execute(args);
+        }
+        private static bool ValidateAndSetOptions(CommandOption accessOption, CommandOption secretOption, CommandOption nameOption,
+            CommandOption regionOption, CommandOption showGoodOption, CommandOption showUnknownOption)
+        {
+            if (!accessOption.HasValue())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("AccessKey is required.");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+            if (!secretOption.HasValue())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("SecretKey is required.");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+            if (!nameOption.HasValue())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Name is required.");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+
+
+            AccessKey = accessOption.Value();
+            SecretKey = secretOption.Value();
+            Name = nameOption.Value();
+            ShowGoodInstances = showGoodOption.HasValue();
+            ShowUnknownInstances = showUnknownOption.HasValue();
+
+            Region = string.IsNullOrWhiteSpace(regionOption.Value()) ? RegionEndpoint.USEast1 : RegionEndpoint.GetBySystemName(regionOption.Value());
+
+            return true;
+        }
+
+        private static async Task<InstanceResults> FindInstancesAndCategorize()
+        {
+            var instances = await FindInstances();
+            AmazonCloudWatchClient cwClient = new AmazonCloudWatchClient(AccessKey, SecretKey, Region);
+
+            var instanceResults = new InstanceResults();
+            
+            await instances.ForEachAsync(MaxMetricConcurrency, async (instance) =>
+            {
+                //var instance = instances[i];
+
+                var dim = new Dimension()
+                {
+                    Name = "InstanceId",
+                    Value = instance,
+                };
+
+                var request = new GetMetricStatisticsRequest()
+                {
+                    Dimensions = new List<Dimension> { dim },
+                    Namespace = "AWS/EC2",
+                    MetricName = "CPUUtilization",
+                    Unit = StandardUnit.Percent,
+                    Statistics = new List<string>() { "Average" },
+                    StartTime = DateTime.UtcNow.Subtract(TimeSpan.FromHours(MetricRetrievalInHours)),
+                    EndTime = DateTime.UtcNow,
+                    Period = (int)TimeSpan.FromMinutes(PeriodInMinutes).TotalSeconds,
+                };
+                var response = await cwClient.GetMetricStatisticsAsync(request);
+                var max = response.Datapoints.Count < MinServerMetricsToJudge ? UnknownCpuPercent : response.Datapoints.Max(p => p.Average);
+
+                if (max == UnknownCpuPercent)
+                {
+                    instanceResults.UnknownInstances.Add(new InstanceResult(instance, max));
+                }
+                else if (max < MaxBadCpuPercent)
+                {
+                    instanceResults.BadInstances.Add(new InstanceResult(instance, max));
+                }
+                else
+                {
+                    instanceResults.GoodInstances.Add(new InstanceResult(instance, max));
+                }
+
+                var processed = instanceResults.BadInstances.Count + instanceResults.UnknownInstances.Count +
+                    instanceResults.GoodInstances.Count;
+
+                Console.Write("\rProcessed {0}/{1} instances.", processed, instances.Count);
+            });
+
+            Console.WriteLine();
+            return instanceResults;
+        }
+
+        private static async Task<List<string>> FindInstances()
+        {
+            AmazonEC2Client client = new AmazonEC2Client(AccessKey, SecretKey, Region);
+
+            //TODO: support paging with tokens
+            var request = new DescribeInstancesRequest()
+            {
+               Filters = new List<Filter>()
+               {
+                   new Filter("tag:Name", new List<string> { Name }),
+                   new Filter("instance-state-name",new List<string> { "running" }),
+               },
+            };
+            var response = await client.DescribeInstancesAsync(request);
+            var instances = response.Reservations.SelectMany(r => r.Instances).Select(i => i.InstanceId).ToList();
+            Console.WriteLine();
+            Console.WriteLine("Found {0} matching instances.", instances.Count);
+            return instances;
+        }
+
+        private static void DisplayInstanceResults(InstanceResults results)
+        {
+            Console.WriteLine("");
+            if (ShowGoodInstances)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Good Instances ({0})\tCPU%", results.GoodInstances.Count);
+                Console.WriteLine("============================");
+                foreach (var result in results.GoodInstances)
+                {
+                    Console.WriteLine(result);
+                }
+                Console.WriteLine("");
+            }
+            if (ShowUnknownInstances)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Unknown Instances ({0})\tCPU%", results.UnknownInstances.Count);
+                Console.WriteLine("============================");
+                foreach (var result in results.UnknownInstances)
+                {
+
+                    Console.WriteLine(result);
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Bad Instances ({0})\tCPU%", results.BadInstances.Count);
+            Console.WriteLine("============================");
+            foreach (var result in results.BadInstances)
+            {
+                Console.WriteLine( result);
+            }
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+
+    }
+    internal static class AsyncHelpers
+    {
+        //from: http://blogs.msdn.com/b/pfxteam/archive/2012/03/05/10278165.aspx
+        internal static Task ForEachAsync<T>(
+            this IEnumerable<T> source, int dop, Func<T, Task> body)
+        {
+            return Task.WhenAll(
+                from partition in Partitioner.Create(source).GetPartitions(dop)
+                select Task.Run(async delegate {
+                    using (partition)
+                        while (partition.MoveNext())
+                            await body(partition.Current).ContinueWith(t =>
+                            {
+                                //observe exceptions
+                            });
+
+                }));
+        }
+    }
+
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("find-idle-instances")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("find-idle-instances")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f86e7ed-0231-4679-93e9-66064f1f510a")]
